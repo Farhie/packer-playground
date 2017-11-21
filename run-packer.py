967752395a7a32bc70ff9255480194228fd87d25
Adam Fahie <adam.fahie@gmail.com>
2017-11-20 22:03:25 -0800

    Adam - Python script for running packer and improved variable naming

--- run-packer.py
+++ run-packer.py
@@ -3,20 +3,33 @@ from random import randint
 import boto3
 import subprocess
 
-key_name = 'packer-key-pair-{}'.format(randint(10000, 1000000000000000))
-private_key_filename = '{}.pem'.format(key_name)
+import os
 
-ec2 = boto3.client('ec2')
-response = ec2.create_key_pair(KeyName=key_name)
-private_key = response['KeyMaterial']
 
-f = open(private_key_filename, 'w')
-f.write(private_key)
-f.close()
+def generate_private_key(ec2_client, ec2_key_pair_name, private_key_filename):
+    response = ec2_client.create_key_pair(KeyName=ec2_key_pair_name)
+    f = open(private_key_filename, 'w')
+    f.write(response['KeyMaterial'])
+    f.close()
+    os.chmod(private_key_filename, 400)
 
-subprocess.run(['chmod', '400', private_key_filename])
 
-subprocess.run(['packer', 'build', '-var', 'private_key={}'.format(private_key_filename),
-                '-var', 'instance_user={}'.format(key_name), 'amazon-linux.json'], check=True)
+def packer_build(private_key_filename, ec2_key_pair_name):
+    subprocess.run(['packer', 'build', '-var', 'private_key_location={}'.format(private_key_filename),
+                    '-var', 'ec2_key_pair_name={}'.format(ec2_key_pair_name), 'amazon-linux.json'], check=True)
 
-ec2.delete_key_pair(KeyName=key_name)
+
+def main():
+    ec2_client = boto3.client('ec2')
+    ec2_key_pair_name = 'packer-key-pair-{}'.format(randint(10000, 1000000000000000))
+    private_key_filename = 'temporary_key.pem'
+    try:
+        generate_private_key(ec2_client, ec2_key_pair_name, private_key_filename)
+        packer_build(private_key_filename, ec2_key_pair_name)
+    finally:
+        ec2_client.delete_key_pair(KeyName=ec2_key_pair_name)
+        os.remove(private_key_filename)
+
+
+if __name__ == "__main__":
+    main()
