d66d43ecf61d9c55dfa2e9808919d95c9a24a1e1
Adam Fahie <adam.fahie@gmail.com>
2017-11-28 20:06:34 -0800

    Adam - Ensure kernel config is reloaded after each change. Complete 3.1.2 requirements

--- spec/3_network_configuration.rb
+++ spec/3_network_configuration.rb
@@ -8,3 +8,18 @@ control "cis-3.1.1" do
     its('value') { should eq 0 }
   end
 end
+
+
+control "cis-3.1.2" do
+  impact 1.0
+  title ""
+  desc ""
+
+  describe kernel_parameter('net.ipv4.conf.all.send_redirects') do
+    its('value') { should eq 0 }
+  end
+
+  describe kernel_parameter('net.ipv4.conf.default.send_redirects') do
+    its('value') { should eq 0 }
+  end
+end
